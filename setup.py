a4a40a90bd874a7c27bbb4b9050705af62a744c2
DaryabiTB <talibdaryabi@gmail.com>
2023-03-05 11:15:48 +0100

    setup and requrirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e."
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirements.
+    '''
+
+    requiremnets = []
+    with open(file_path) as file_obj:
+        requiremnets = file_obj.readlines()
+        [ req.replace("\n" , "") for req in requiremnets ]
+
+        if HYPHEN_E_DOT in requiremnets:
+            requiremnets.remove(HYPHEN_E_DOT)
+    return requiremnets
+
+setup(
+    name = "MLProject",
+    version="0.0.1",
+    author="Mohammad Talib Daryabi",
+    author_email="talibdaryabi@gmail.com",
+    packages= find_packages(),
+    install_requires = get_requirements('requirements.txt'), 
+    
+)
+
+
